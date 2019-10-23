7390d413c82d66bd070e625171f4e2139813f8f3
YunosukeMatsushita0901 <handmatsu91@gmail.com>
2019-10-23 05:17:36 +0000

    some fix

---
+++ kadai-2/intex.js
@@ -0,0 +1,13 @@
+const name = `mugi`
+console.log (name)
+let name2 = `matu`
+name2 = `matu`
+console.log (name2)
+
+const phrase1 = "i`m a cat."
+console.log(phrase1)
+
+const name = `mike`
+console.log(typeof name)
+name = 3
+console.log(typeof name)
---
+++ kadai-3/index.js
@@ -0,0 +1,14 @@
+const firstname = `yunosuke`
+const secondname = `matsushita`
+console.log(firstname + secondname)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasdog = true
+const hascat = false
+
+console.log(hasdog || hascat)
+console.log(hasdog && hascat)
+
+console.log(!hasdog && !hascat)
